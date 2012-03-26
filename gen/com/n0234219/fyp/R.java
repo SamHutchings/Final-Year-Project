bed5873b28aaf6604d7103924315a1f500f37afe
Sam Hutchings <samhutchings@gmail.com>
2012-03-26 23:22:34 +0100

    more layout files

--- gen/com/n0234219/fyp/R.java
+++ gen/com/n0234219/fyp/R.java
@@ -13,11 +13,25 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int exif=0x7f050001;
+        public static final int mapview=0x7f050003;
+        public static final int photo_fragment=0x7f050004;
+        public static final int photo_item=0x7f050005;
+        public static final int preferences=0x7f050002;
+        public static final int presentation=0x7f050000;
+    }
     public static final class layout {
         public static final int mainmenu=0x7f030000;
+        public static final int map_view=0x7f030001;
+        public static final int mapview_fragment=0x7f030002;
+        public static final int photo_fragment=0x7f030003;
+        public static final int photo_item=0x7f030004;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f040000;
+        public static final int exif_button=0x7f040002;
+        public static final int options_button=0x7f040003;
+        public static final int presentation_button=0x7f040001;
     }
 }
