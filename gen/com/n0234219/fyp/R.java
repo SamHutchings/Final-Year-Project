07e9b26e340b0788c7fe98a907fe4fbb918ac469
Sam Hutchings <samhutchings@gmail.com>
2012-03-27 13:10:13 +0100

    implemented first version of presentation menu

--- gen/com/n0234219/fyp/R.java
+++ gen/com/n0234219/fyp/R.java
@@ -16,11 +16,14 @@ public final class R {
     }
     public static final class id {
         public static final int exif=0x7f050003;
+        public static final int filechoice=0x7f050006;
+        public static final int filechoicetext=0x7f050007;
         public static final int mapview=0x7f050005;
         public static final int mapview_fragment=0x7f050001;
-        public static final int photo_item=0x7f050006;
+        public static final int photo_item=0x7f050009;
         public static final int preferences=0x7f050004;
         public static final int presentation=0x7f050002;
+        public static final int startpresentation=0x7f050008;
         public static final int thumbnail_fragment=0x7f050000;
     }
     public static final class layout {
@@ -28,13 +31,17 @@ public final class R {
         public static final int mainmenu=0x7f030001;
         public static final int map_view=0x7f030002;
         public static final int mapview_fragment=0x7f030003;
-        public static final int thumbnail_fragment=0x7f030004;
-        public static final int thumbnail_view=0x7f030005;
+        public static final int presentation_menu=0x7f030004;
+        public static final int thumbnail_fragment=0x7f030005;
+        public static final int thumbnail_view=0x7f030006;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
         public static final int exif_button=0x7f040002;
+        public static final int file_choice_button=0x7f040004;
+        public static final int file_choice_label=0x7f040006;
         public static final int options_button=0x7f040003;
         public static final int presentation_button=0x7f040001;
+        public static final int start_presentation_button=0x7f040005;
     }
 }
--- src/com/n0234219/fyp/PresentationMenuActivity.java
+++ src/com/n0234219/fyp/PresentationMenuActivity.java
@@ -1,7 +1,39 @@
 package com.n0234219.fyp;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class PresentationMenuActivity extends Activity {
+	
+	private static final int PICK_FROM_FILE = 2;
+	
+	@Override
+	public void onCreate(Bundle bundle) {
+	 super.onCreate(bundle);
+     setContentView(R.layout.presentation_menu);      
+     final Button fileChoiceButton = (Button) findViewById(R.id.filechoice);
+     fileChoiceButton.setOnClickListener(new View.OnClickListener() {
+          public void onClick(View v) {
+        	  Intent intent = new Intent();
+        	  
+              intent.setType("image/*");
+              intent.setAction(Intent.ACTION_GET_CONTENT);
+
+              startActivityForResult(Intent.createChooser(intent, "Complete action using"), PICK_FROM_FILE);
+          }
+      });
+     final Button startPresentationButton = (Button) findViewById(R.id.startpresentation);
+     startPresentationButton.setOnClickListener(new View.OnClickListener() {
+          public void onClick(View v) {
+         	 Intent myIntent = new Intent(PresentationMenuActivity.this, PresentationActivity.class);
+         	 PresentationMenuActivity.this.startActivity(myIntent);
+          }
+      });
+     final TextView fileChoiceText = (TextView) findViewById(R.id.filechoicetext);
+	}
 
 }
