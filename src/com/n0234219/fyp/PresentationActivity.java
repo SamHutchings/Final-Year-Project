bb3d3db1b5d990c017057ef167305409b6cfc9e9
Sam <Sam@Sam-Laptop>
2012-03-28 18:29:49 +0100

    added layouts for presentation and begun implementing

--- src/com/n0234219/fyp/PresentationActivity.java
+++ src/com/n0234219/fyp/PresentationActivity.java
@@ -1,5 +1,29 @@
 package com.n0234219.fyp;
 
-public class PresentationActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 
+public class PresentationActivity extends Activity {
+
+	private String fileName;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.presentation);
+		PhotoViewFragment viewer = (PhotoViewFragment) getFragmentManager()
+				.findFragmentById(R.id.photo);
+
+		if (viewer == null || !viewer.isInLayout()) {
+			Intent showContent = new Intent(getApplicationContext(),
+					MapViewActivity.class);
+			
+			startActivity(showContent);	
+
+		} else {
+			
+		}
+
+	}
 }
--- src/com/n0234219/fyp/PresentationMenuActivity.java
+++ src/com/n0234219/fyp/PresentationMenuActivity.java
@@ -11,44 +11,47 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class PresentationMenuActivity extends Activity {
-	
+
 	private ArrayList<Uri> selectedImages;
-	private Uri mImageCaptureUri;
+	private Uri mImageCaptureUri = null;
 	private static final int PICK_FROM_FILE = 2;
-	
+
 	@Override
 	public void onCreate(Bundle bundle) {
-	 super.onCreate(bundle);
-     setContentView(R.layout.presentation_menu);      
-     final Button fileChoiceButton = (Button) findViewById(R.id.filechoice);
-     fileChoiceButton.setOnClickListener(new View.OnClickListener() {
-          public void onClick(View v) {
-        	  Intent intent = new Intent();
-        	  
-              intent.setType("image/*");
-              intent.setAction(Intent.ACTION_GET_CONTENT);
+		super.onCreate(bundle);
+		setContentView(R.layout.presentation_menu);      
+		final Button fileChoiceButton = (Button) findViewById(R.id.filechoice);
+		fileChoiceButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent();
 
-              startActivityForResult(Intent.createChooser(intent, "Complete action using"), PICK_FROM_FILE);
-          }
-      });
-     final Button startPresentationButton = (Button) findViewById(R.id.startpresentation);
-     startPresentationButton.setOnClickListener(new View.OnClickListener() {
-          public void onClick(View v) {
-         	 Intent myIntent = new Intent(PresentationMenuActivity.this, PresentationActivity.class);
-         	 PresentationMenuActivity.this.startActivity(myIntent);
-          }
-      });
-     final TextView fileChoiceText = (TextView) findViewById(R.id.filechoicetext);
+				intent.setType("image/*");
+				intent.setAction(Intent.ACTION_GET_CONTENT);
+
+				startActivityForResult(Intent.createChooser(intent, "Complete action using"), PICK_FROM_FILE);
+			}
+		});
+		final Button startPresentationButton = (Button) findViewById(R.id.startpresentation);
+		startPresentationButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				if(null != mImageCaptureUri) {
+					Intent myIntent = new Intent(PresentationMenuActivity.this, PresentationActivity.class);
+
+					PresentationMenuActivity.this.startActivity(myIntent);
+				}
+			}
+		});
+		final TextView fileChoiceText = (TextView) findViewById(R.id.filechoicetext);
 	}
-	
+
 	@Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode != RESULT_OK) return;
- 
-       if (requestCode == PICK_FROM_FILE) {
-            mImageCaptureUri = data.getData();
-            
-        }
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (resultCode != RESULT_OK) return;
+
+		if (requestCode == PICK_FROM_FILE) {
+			mImageCaptureUri = data.getData();
+
+		}
 
 	}
 }
