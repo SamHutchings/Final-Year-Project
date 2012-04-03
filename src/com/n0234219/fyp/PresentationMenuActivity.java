9a9b9b2b41984e1f4cc83b5597bcaec2a2a4b043
Sam Hutchings <samhutchings@gmail.com>
2012-04-03 17:15:03 +0100

    committing all changes

--- src/com/n0234219/fyp/PresentationMenuActivity.java
+++ src/com/n0234219/fyp/PresentationMenuActivity.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,7 +17,8 @@ public class PresentationMenuActivity extends Activity {
 	private ArrayList<Uri> selectedImages;
 	private TextView fileChoiceText;
 	private Uri mImageCaptureUri = null;
-	private static final int PICK_FROM_FILE = 2;
+	private String filePath = null;
+	private static final int PICK_FROM_FILE = 1;
 
 	@Override
 	public void onCreate(Bundle bundle) {
@@ -35,9 +38,9 @@ public class PresentationMenuActivity extends Activity {
 		final Button startPresentationButton = (Button) findViewById(R.id.startpresentation);
 		startPresentationButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				if(null != mImageCaptureUri) {
+				if(null != filePath) {
 					Intent myIntent = new Intent(PresentationMenuActivity.this, PresentationActivity.class);
-
+					myIntent.putExtra("Path", filePath);
 					PresentationMenuActivity.this.startActivity(myIntent);
 				}
 			}
@@ -52,10 +55,24 @@ public class PresentationMenuActivity extends Activity {
 		if (requestCode == PICK_FROM_FILE) {
 			if(!data.getData().equals(null)) {
 				mImageCaptureUri = data.getData();
-				fileChoiceText.setText(mImageCaptureUri.toString() + " selected.");
+				filePath = getRealPathFromURI(mImageCaptureUri);
+				fileChoiceText.setText(filePath + " selected.");
 
 			}
 
 		}
 	}
+	
+	  public String getRealPathFromURI(Uri contentUri) {
+	        String [] proj      = {MediaStore.Images.Media.DATA};
+	        Cursor cursor       = managedQuery( contentUri, proj, null, null,null);
+	 
+	        if (cursor == null) return null;
+	 
+	        int column_index    = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+	 
+	        cursor.moveToFirst();
+	 
+	        return cursor.getString(column_index);
+	    }
 }
