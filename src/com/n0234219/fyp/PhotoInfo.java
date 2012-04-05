d6b67f0c7398bc287ffc478df6abeb64bc8e404c
Sam <Sam@Sam-Laptop>
2012-04-05 15:45:39 +0100

    photoinfo updates

--- src/com/n0234219/fyp/PhotoInfo.java
+++ src/com/n0234219/fyp/PhotoInfo.java
@@ -3,8 +3,43 @@ package com.n0234219.fyp;
 import android.text.format.Time;
 
 public class PhotoInfo {
-	private double latitude;
-	private double longitude;
+	private Double latitude;
+	private Double longitude;
 	private String location;
 	private Time timeTaken;
+	
+	public Double getLatitude() {
+		return latitude;
+	}
+	
+	public void setLatitude(Double lat) {
+		latitude = lat;
+	}
+	
+	public Double getLongitude() {
+		return longitude;
+	}
+	
+	public void setLongitude(Double lng) {
+		longitude = lng;
+	}
+	
+	public String getLocation() {
+		return location;
+	}
+	
+	public void setLocation(String loc) {
+		location = loc;
+	}
+	
+	public Time getTimeTaken() {
+		return timeTaken;
+	}
+	
+	public void setTimeTaken(Time time) {
+		timeTaken = time;
+	}
+	
+	
+	
 }
