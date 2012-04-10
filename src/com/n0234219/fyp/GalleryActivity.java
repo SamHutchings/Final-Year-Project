9de215eb72833a19262f40e7e3bc83a179c3b007
Sam Hutchings <samhutchings@gmail.com>
2012-04-10 16:56:48 +0100

    new layout files for gallery selection

---
+++ src/com/n0234219/fyp/GalleryActivity.java
@@ -0,0 +1,13 @@
+package com.n0234219.fyp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GalleryActivity extends Activity {
+	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.gallery_view);
+	}
+
+}
---
+++ src/com/n0234219/fyp/ThumbnailViewActivity.java
@@ -0,0 +1,13 @@
+package com.n0234219.fyp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ThumbnailViewActivity extends Activity {
+	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.thumbnail_fragment);
+	}
+	
+}
