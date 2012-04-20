cb796f5bdc694a2741f617049b8ad51d8c7a84b4
Sam Hutchings <samhutchings@gmail.com>
2012-04-20 17:55:42 +0100

    show photographs newest first - update map icon

--- src/com/n0234219/fyp/GalleryActivity.java
+++ src/com/n0234219/fyp/GalleryActivity.java
@@ -65,7 +65,7 @@ public class GalleryActivity extends Activity implements LoaderManager.LoaderCal
 		CursorLoader cursorLoader = new CursorLoader(this,
 				MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection,
 				 MediaStore.Images.Media.DATA + " like ? ",
-			        new String[] {"%Camera%"}, null);
+			        new String[] {"%Camera%"}, MediaStore.Images.ImageColumns.DATE_TAKEN + " desc");
 		return cursorLoader;
 	}
 
