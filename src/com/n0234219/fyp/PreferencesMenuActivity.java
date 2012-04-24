18c6cc42d51a33d69fa1ef9601d7f6f8d2b9b1cc
Sam Hutchings <samhutchings@gmail.com>
2012-04-24 11:22:30 +0100

    compatibility and preferences

--- src/com/n0234219/fyp/PreferencesMenuActivity.java
+++ src/com/n0234219/fyp/PreferencesMenuActivity.java
@@ -2,6 +2,7 @@ package com.n0234219.fyp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.preference.PreferenceActivity;
 
 
@@ -10,5 +11,6 @@ public class PreferencesMenuActivity extends PreferenceActivity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.preferences_menu);
+	
 	}
 }
